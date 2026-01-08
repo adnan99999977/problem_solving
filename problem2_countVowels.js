a5c7d14edd4293629f803c65f660eaf0e0e77486
Naif Adnan <naifadnan999@gmail.com>
2026-01-08 17:27:56 +0600

    solve second js problem count vowels

---
+++ problem2_countVowels.js
@@ -0,0 +1,20 @@
+// count vowels
+function countVowels(str) {
+  let count = 0;
+  let vowels = "aeiouAEIOU";
+
+  for (let char of str) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(countVowels("programming"));
+
+// shortcut version
+function countVowels(str) {
+  return str.split("").filter((ch) => "aeiouAEIOU".includes(ch)).length;
+}
+
+console.log(countVowels("programming"));
