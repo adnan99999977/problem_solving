42233a9acc9ccfcd1f86c5f3afd5b0cdae5a8368
Naif Adnan <naifadnan999@gmail.com>
2026-01-08 17:23:01 +0600

    solve first js problem revers a string

---
+++ problem1_reverseString.js
@@ -0,0 +1,16 @@
+// first method to revers a string
+function reverseString(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+console.log(reverseString("hello")); // olleh
+
+
+// second method to revers a string
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+console.log(reverseString("hello")); // olleh
